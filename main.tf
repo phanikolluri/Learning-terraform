9e32018b76862363735ee5e6ecada1a11af6c44f
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-03-24 11:51:02 +0530

    terraform learning

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "x" {
+  default = 20
+}
+
+variable "b" {
+  default = "xyz"
+}
+
+variable "example" {
+  a = 123
+  b = "check"
+  c = true
+}
+
+
+output "x" {
+  value = var.x
+}
+
+output "b" {
+  value = "b value is ${var.b}"
+}
