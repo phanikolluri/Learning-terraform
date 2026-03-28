c450e236bf2f9f09814da96cd93c74d3cf4e1459
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-03-28 20:07:13 +0530

    terraform learning

---
+++ 01-provisioners/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "instance" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0ce01673ed9893e29"]
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = self.public_ip
+      user = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "echo today we are learning provisoner concept"
+    ]
+  }
+}
